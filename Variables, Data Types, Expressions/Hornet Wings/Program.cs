b96a38b182a6874a0bf741f04151a8ec0c168ad9
Raya Nikolova- Kirilova <32265273+rayaKirilova@users.noreply.github.com>
2018-11-15 15:31:27 +0200

    Add files via upload

---
+++ Variables, Data Types, Expressions/Hornet Wings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hornet_Wings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wingFlaps = int.Parse(Console.ReadLine());
+            double distanceInMeters = double.Parse(Console.ReadLine());
+            int endurance = int.Parse(Console.ReadLine());
+
+            int rests = wingFlaps / endurance; // how many rests the hornet will have
+            long secondsPassed = rests * 5L; // total seconds the hornet will rest / using long 
+
+
+            double metersTravelled = (wingFlaps / 1000) * distanceInMeters ;
+            secondsPassed += wingFlaps / 100 ;
+
+            Console.WriteLine($"{metersTravelled:F2} m.");
+            Console.WriteLine($"{secondsPassed:F0} s.");
+        }
+    }
+}
