7537f32fea9e24bae49d4ddbfaa26928bacbe671
Raya Nikolova- Kirilova <32265273+rayaKirilova@users.noreply.github.com>
2018-11-20 08:41:20 +0200

    Add files via upload

---
+++ Conditional Statements and Loops/Number Pyramid/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int count = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (count <= n)
+                    {
+                        Console.Write(count + " ");
+                        count++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Conditional Statements and Loops/Numbers from 2^0 to 2^N/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbers_from_2_0_to_2_N
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int number = 1;
+
+            for (int i = 0; i <= n; i++)
+            {
+                Console.WriteLine(number);
+                number *= 2;
+            }
+        }
+    }
+ }
