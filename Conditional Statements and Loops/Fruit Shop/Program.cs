ad6a02a49a026856e6d9e3e69b3c16dd0b1ae5be
Raya Nikolova- Kirilova <32265273+rayaKirilova@users.noreply.github.com>
2018-11-20 08:37:30 +0200

    Add files via upload

---
+++ Conditional Statements and Loops/Fruit Shop/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fruit_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fruit = Console.ReadLine().ToLower();
+            string dayOfWeek = Console.ReadLine().ToLower();
+            double quantity = double.Parse(Console.ReadLine());
+
+            bool workingDays = dayOfWeek == "monday" || dayOfWeek == "tuesday" || dayOfWeek == "wednesday"
+            || dayOfWeek == "thursday" || dayOfWeek == "friday";
+
+            bool daysOff = dayOfWeek == "saturday" || dayOfWeek == "sunday";
+
+            double price = 0;
+
+            if (fruit == "banana")
+            {
+                if (workingDays)
+                {
+                    price = quantity * 2.50;
+                    Console.WriteLine($"{price:F2}");
+                }
+                else if (daysOff)
+                {
+                    price = quantity * 2.70;
+                    Console.WriteLine($"{price:F2}");
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+            else if (fruit == "apple")
+            {
+                if (workingDays)
+                {
+                    price = quantity * 1.20;
+                    Console.WriteLine($"{price:F2}");
+                }
+                else if (daysOff)
+                {
+                    price = quantity * 1.25;
+                    Console.WriteLine($"{price:F2}");
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+            else if (fruit == "orange")
+            {
+                if (workingDays)
+                {
+                    price = quantity * 0.85;
+                    Console.WriteLine($"{price:F2}");
+                }
+                else if (daysOff)
+                {
+                    price = quantity * 0.90;
+                    Console.WriteLine($"{price:F2}");
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+            else if (fruit == "grapefruit")
+            {
+                if (workingDays)
+                {
+                    price = quantity * 1.45;
+                    Console.WriteLine($"{price:F2}");
+                }
+                else if (daysOff)
+                {
+                    price = quantity * 1.60;
+                    Console.WriteLine($"{price:F2}");
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+            else if (fruit == "kiwi")
+            {
+                if (workingDays)
+                {
+                    price = quantity * 2.70;
+                    Console.WriteLine($"{price:F2}");
+                }
+                else if (daysOff)
+                {
+                    price = quantity * 3.00;
+                    Console.WriteLine($"{price:F2}");               
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+            else if (fruit == "pineapple")
+            {
+                if (workingDays)
+                {
+                    price = quantity * 5.50;
+                    Console.WriteLine($"{price:F2}");
+                }
+                else if (daysOff)
+                {
+                    price = quantity * 5.60;
+                    Console.WriteLine($"{price:F2}");
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+            else if (fruit == "grapes")
+            {
+                if (workingDays)
+                {
+                    price = quantity * 3.85;
+                    Console.WriteLine($"{price:F2}");
+                }
+                else if (daysOff)
+                {
+                    price = quantity * 4.20;
+                    Console.WriteLine($"{price:F2}");
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+        }
+    }
+}
+   
---
+++ Conditional Statements and Loops/Left and Right Sum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Left_and_Right_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int leftSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                leftSum += int.Parse(Console.ReadLine());
+            }
+
+            int rightSum = 0;
+
+            for (int i = n; i < n * 2; i++)
+            {
+                rightSum += int.Parse(Console.ReadLine());
+            }
+
+            if (leftSum == rightSum)
+            {
+                Console.WriteLine($"Yes, sum = {leftSum}");
+            }
+            else
+            {
+                Console.WriteLine($"No, diff = {Math.Abs(leftSum - rightSum)}");
+            }
+        }
+    }
+}
