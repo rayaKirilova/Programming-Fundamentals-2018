068e1c785f1dc941d5d6f0a0422cf3123043292f
Raya Nikolova- Kirilova <32265273+rayaKirilova@users.noreply.github.com>
2018-12-06 18:20:30 +0200

    Add files via upload

---
+++ Arrayas and Lists/Last K Numbers Sums Sequence/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Last_K_Numbers_Sums_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            long[] sequence = new long[n];
+
+            sequence[0] = 1;
+
+            for (int i = 1; i < n; i++)
+            {
+                long sum = 0;
+
+                int startIndex = Math.Max(0, i - k);
+
+                for (int j = startIndex; j < i; j++)
+                {
+                    sum += sequence[j];
+                }
+
+                sequence[i] = sum;
+            }
+            Console.WriteLine(string.Join(" ", sequence));
+
+        }
+    }
+}
