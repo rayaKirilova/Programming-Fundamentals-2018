234d665356875560a062f08d7de9686fc0dac8d3
Raya Nikolova- Kirilova <32265273+rayaKirilova@users.noreply.github.com>
2018-12-13 18:12:40 +0200

    Add files via upload

---
+++ Arrayas and Lists/Search for a Number/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search_for_a_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().
+                Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).
+                Select(int.Parse).
+                ToList();
+
+            int taker = 0;
+            int reaper = 0;
+            int wanted = 0;
+            List<int> takenNum = new List<int>();
+
+            int[] saintThree = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            reaper = saintThree[1];
+            wanted = saintThree[2];
+
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                takenNum.Add(numbers[i]);
+            }
+
+            takenNum.RemoveRange(0, reaper);
+
+            if (takenNum.Contains(wanted))
+            {
+                Console.WriteLine("YES!");
+            }
+            else
+            {
+                Console.WriteLine("NO!");
+            }
+        }
+    }
+}
---
+++ Arrayas and Lists/Sum Arrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] secondArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int maxLength = Math.Max(firstArray.Length, secondArray.Length); 
+
+            int[] sumArray = new int[maxLength];
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                sumArray[i] = firstArray[i % firstArray.Length] + secondArray[i % secondArray.Length];
+            }
+
+            Console.WriteLine(string.Join(" ", sumArray));
+
+        }
+
+    }
+    }
