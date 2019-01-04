ab9a7aff131df1eff25d96b26436a58c18bd5449
Raya Nikolova- Kirilova <32265273+rayaKirilova@users.noreply.github.com>
2019-01-04 12:01:49 +0200

    Add files via upload

---
+++ Classes. Constructors. Data. Methods/Sign of Integer Number/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sign_of_Integer_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            CheckSign(number); 
+
+        }
+
+        static void CheckSign(int n)
+        {
+            if (n > 0)
+            {
+                Console.WriteLine($"The number {n} is positive.");
+            }
+            else if (n < 0)
+            {
+                Console.WriteLine($"The number {n} is negative.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {n} is zero.");
+            }
+
+        }
+    }
+}
---
+++ Classes. Constructors. Data. Methods/Sum of Digits Divisible By 7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_Digits_Divisible_By_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            bool sumDivisibleBy7 = false;
+
+            int sum = 0; 
+
+            while (number > 0)
+            {
+
+                int digit = number % 10;
+
+                sum += digit;
+
+                number /= 10;
+            }
+
+            if (sum % 7 == 0)
+            {
+                sumDivisibleBy7 = true;
+            }
+
+            Console.WriteLine(sumDivisibleBy7);
+        }
+    }
+}
